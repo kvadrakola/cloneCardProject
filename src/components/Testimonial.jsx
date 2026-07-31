469f8d55d06007d14266bc071f8c6df985ba19f3
kvadrakola <kvadrakola@gmail.com>
2026-08-01 00:27:52 +0200

    style:add custom style in Testimonial css

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -2,29 +2,29 @@ import '../styles/Testimonial.css';
 
 function Testimonial(props) {
     return (
-    <div className='testimonial-container'>
-        <img
-        className='testimonial-image'
-        src={props.image}
-        alt={`Photo of ${props.name}`}
-        />
+        <div className='testimonial-container'>
+            <img
+                className='testimonial-image'
+                src={props.image}
+                alt={`Photo of ${props.name}`}
+            />
 
-    <div className='testimonial-text-container'>
-        <p className='testimonial-name'>
-            <strong>{props.name}</strong> in {props.country}
-        </p>
+            <div className='testimonial-text-container'>
+                <p className='testimonial-name'>
+                    <strong>{props.name}</strong> in {props.country}
+                </p>
 
-        <p className='testimonial-job'>
-            {props.jobTitle} in <strong>{props.jobCompany}</strong>
-        </p>
+                <p className='testimonial-job'>
+                    {props.jobTitle} at <strong>{props.jobCompany}</strong>
+                </p>
 
-        <p className='testimonial-text'>
-            "{props.text}"
-        </p>
-            
-    </div>
-    </div>
-            );
-        }
+                <p className='testimonial-text'>
+                    "{props.text}"
+                </p>
+
+            </div>
+        </div>
+    );
+}
 
 export default Testimonial;
