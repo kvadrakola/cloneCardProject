5827f23950a82b6e21e9649f8e9a68a6937454b9
kvadrakola <kvadrakola@gmail.com>
2026-08-01 00:23:48 +0200

    fix: props text

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const testimonials = [
     jobTitle: 'Software Engineer',
     jobCompany: 'Amazon',
     image: ShawnPhoto,
-    quote: (
+    text: (
       <>
         &ldquo;It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. <strong>freeCodeCamp changed my life.</strong>&rdquo;
       </>
@@ -24,7 +24,7 @@ const testimonials = [
     jobTitle: 'Software Engineer',
     jobCompany: 'ChatDesk',
     image: SarahPhoto,
-    quote: (
+    text: (
       <>
         &ldquo;<strong>freeCodeCamp was the gateway to my career</strong> as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company.&rdquo;
       </>
@@ -36,7 +36,7 @@ const testimonials = [
     jobTitle: 'Software Engineer',
     jobCompany: 'Spotify',
     image: EmmaPhoto,
-    quote: (
+    text: (
       <>
         &ldquo;I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on <strong>freeCodeCamp gave me the skills</strong> and confidence I needed to land my dream job as a software engineer at Spotify.&rdquo;
       </>
